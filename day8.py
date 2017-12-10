29cb9cb1d77d6138954c0e6b95bd9eaed8203195
Erik Brännström <erik.brannstrom@gmail.com>
2017-12-10 08:07:11 +0100

    Day 8: I Heard You Like Registers [Star 2]
    Nothin weird here.

--- day8.py
+++ day8.py
@@ -13,18 +13,30 @@ def operator_to_func(op):
 	}
 	return operators.get(op)
 
-def first(input):
+def apply_line(line, registers):
 	regex = re.compile(r"([a-z]+) ([a-z]+) (-?\d+) if ([a-z]+) ([<>=!]+) (-?\d+)")
+	matches = regex.match(line)
+	(reg, op, val, reg_comp, comp, val_comp) = matches.groups()
+	if operator_to_func(comp)(registers.get(reg_comp, 0), int(val_comp)):
+		registers[reg] = operator_to_func(op)(registers.get(reg, 0), int(val))
 
+	return registers
+
+
+def first(input):
 	lines = input.split("\n")
 	registers = {}
 	for line in lines:
-		matches = regex.match(line)
-		(reg, op, val, reg_comp, comp, val_comp) = matches.groups()
-
-		if not operator_to_func(comp)(registers.get(reg_comp, 0), int(val_comp)):
-			continue
-
-		registers[reg] = operator_to_func(op)(registers.get(reg, 0), int(val))
+		apply_line(line, registers)
 
 	return max(registers.values())
+
+def second(input):
+	lines = input.split("\n")
+	registers = {}
+	max_value = 0
+	for line in lines:
+		apply_line(line, registers)
+		max_value = max([max_value] + list(registers.values()))
+
+	return max_value
--- test_data.py
+++ test_data.py
@@ -64,5 +64,11 @@ day8 = (
 		c dec -10 if a >= 1
 		c inc -20 if c == 10"""), 1)
 	],
-	[]
+	[
+		(textwrap.dedent("""\
+		b inc 5 if a > 1
+		a inc 1 if b < 5
+		c dec -10 if a >= 1
+		c inc -20 if c == 10"""), 10)
+	]
 )
