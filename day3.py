b362263fe0588836d87b954220be1fbb98af6c53
Erik Brännström <erik.brannstrom@gmail.com>
2017-12-11 22:06:44 +0100

    Day 3: Spiral Memory [Star 2]
    Took a while, but once I realized I could use tuples as keys in a
    dict it was fairly straight forward.

--- day3.py
+++ day3.py
@@ -1,13 +1,16 @@
-def get_coordinates(square):
+DIRECTION = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+def first(input):
+    square = int(input)
     coords = (0, 0)
 
-    direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     dir_index = 0
     steps_until_increase = 2
     steps_taken = 0
     for current_square in range(1, square):
-        coords = (coords[0] + direction[dir_index][0],
-                  coords[1] + direction[dir_index][1])
+        coords = (coords[0] + DIRECTION[dir_index][0],
+                  coords[1] + DIRECTION[dir_index][1])
 
         steps_taken = steps_taken + 1
         if steps_taken == steps_until_increase / 2:
@@ -17,10 +20,44 @@ def get_coordinates(square):
             steps_until_increase = steps_until_increase + 2
             steps_taken = 0
 
-    return coords
-
-
-def first(input):
-    square = int(input)
-    coords = get_coordinates(square)
     return abs(coords[0]) + abs(coords[1])
+
+
+def second(input):
+    val = int(input)
+    max_value = 1
+    matrix_dict = {
+        (0, 0): 1
+    }
+    coords = (0, 0)
+
+    def sum_neighbors(coords):
+        return sum([matrix_dict.get((coords[0] + 1, coords[1] + 1), 0),
+            matrix_dict.get((coords[0] + 1, coords[1]), 0),
+            matrix_dict.get((coords[0] + 1, coords[1] - 1), 0),
+            matrix_dict.get((coords[0], coords[1] + 1), 0),
+            matrix_dict.get((coords[0], coords[1] - 1), 0),
+            matrix_dict.get((coords[0] - 1, coords[1] + 1), 0),
+            matrix_dict.get((coords[0] - 1, coords[1]), 0),
+            matrix_dict.get((coords[0] - 1, coords[1] - 1), 0)])
+
+    dir_index = 0
+    steps_until_increase = 2
+    steps_taken = 0
+    current_square = 1
+    while max_value <= val:
+        coords = (coords[0] + DIRECTION[dir_index][0],
+                  coords[1] + DIRECTION[dir_index][1])
+
+        matrix_dict[coords] = sum_neighbors(coords)
+        max_value = matrix_dict[coords]
+
+        steps_taken = steps_taken + 1
+        if steps_taken == steps_until_increase / 2:
+            dir_index = (dir_index + 1) % 4
+        if steps_taken == steps_until_increase:
+            dir_index = (dir_index + 1) % 4
+            steps_until_increase = steps_until_increase + 2
+            steps_taken = 0
+
+    return max_value
